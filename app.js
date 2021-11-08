c0888fa42f94f3b702fbcd743d2cc4755bf84032
jaleyerdelen <jaleyerdelen@gmail.com>
2021-11-08 15:25:38 +0300

    added upload file

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const fileUpload = require('express-fileupload');
 const ejs = require('ejs');
 const path = require('path');
+const fs = require('fs');
 const Photo = require('./models/Photo');
 
 const app = express();
-
 
 //connect DB
 mongoose.connect('mongodb://localhost/pcat-test-db', {
@@ -18,25 +19,26 @@ app.set('view engine', 'ejs');
 
 //MIDDLEWARES
 app.use(express.static('public'));
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(fileUpload());
 //ROUTES
 app.get('/', async (req, res) => {
-  const photos = await Photo.find({})
+  const photos = await Photo.find({}).sort('-dateCreated');
   res.render('index', {
-    photos
+    photos,
   });
 });
 app.get('/about', (req, res) => {
   res.render('about');
 });
 
-app.get('/photos/:id', async(req, res) => {
+app.get('/photos/:id', async (req, res) => {
   //console.log(req.params.id);
   const photo = await Photo.findById(req.params.id);
   res.render('photo', {
-    photo
-  })
+    photo,
+  });
 });
 
 app.get('/add', (req, res) => {
@@ -44,9 +46,27 @@ app.get('/add', (req, res) => {
 });
 
 app.post('/photos', async (req, res) => {
-  // console.log(req.body);
-  await Photo.create(req.body)
-  res.redirect('/')
+  // console.log(req.files.image);
+  //await Photo.create(req.body)
+  //res.redirect('/')
+
+  const uploadDir = "public/uploads";
+
+  if(!fs.existsSync(uploadDir)) {
+    fs.mkdirSync(uploadDir)
+  }
+
+  let uploadeImage = req.files.image;
+  let uploadPath = __dirname + '/public/uploads/' + uploadeImage.name;
+
+  uploadeImage.mv(uploadPath,
+     async () => {
+    await Photo.create({
+      ...req.body,
+      image: '/uploads/' + uploadeImage.name,
+    });
+    res.redirect('/');
+  });
 });
 
 const port = 3000;
